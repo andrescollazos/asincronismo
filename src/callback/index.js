64808c353b2b1e64a0b14e03439443cebd856dbf
andrescollazos <afcollazosr@gmail.com>
2020-03-12 18:15:28 -0500

    3: Implementando el callback para obtener información de la API de Rick and Morty
    1- Realiza petición a la api para obtener información global
    2- Realiza petición para obtener personaje
    3- Realiza petición para obtener dimensión

--- src/callback/index.js
+++ src/callback/index.js
@@ -1,4 +1,6 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+const URL_API = 'https://rickandmortyapi.com/api/character/'
+
 
 // Realiza peticiones a una API
 function fetchData(url_api, callback) {
@@ -18,3 +20,18 @@ function fetchData(url_api, callback) {
     }
     xhttp.send()
 }
+
+
+fetchData(URL_API, function(error1, data1) {
+    if (error1) return console.log(error1)
+    fetchData(URL_API + data1.results[0].id, function(error2, data2) {
+        if (error2) return console.log(error2)
+        fetchData(data2.origin.url, function(error3, data3) {
+            if (error3) return console.log(error3)
+            console.log(`Personages ${data1.info.count}`)
+            console.log(data2.name)
+            console.log(data3.dimension)
+        })
+    })
+
+})
